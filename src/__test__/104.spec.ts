945ba4bd7c1483be214435e8de7c53808dfda74d
Jayden12138 <18993882884@163.com>
2024-04-11 11:47:11 +0800

    104

---
+++ src/__test__/104.spec.ts
@@ -0,0 +1,17 @@
+import { maxDepth, TreeNode } from '../../104.二叉树的最大深度'
+
+describe('first', () => {
+	it('maxDepth 1', () => {
+		const root = new TreeNode(
+			3,
+			new TreeNode(9),
+			new TreeNode(20, new TreeNode(15), new TreeNode(7))
+		)
+		expect(maxDepth(root)).toBe(3)
+	})
+
+	it('maxDepth 2', () => {
+		const root = new TreeNode(1, null, new TreeNode(2))
+		expect(maxDepth(root)).toBe(2)
+	})
+})
