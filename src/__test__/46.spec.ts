a25a077edd43f92470fbbb1115719ebd02f82e7e
Jayden12138 <18993882884@163.com>
2024-04-12 15:36:31 +0800

    46

---
+++ src/__test__/46.spec.ts
@@ -0,0 +1,14 @@
+import { permute } from '../../46.全排列'
+
+describe('first', () => {
+	it('permute', () => {
+		expect(permute([1, 2, 3])).toEqual([
+			[1, 2, 3],
+			[1, 3, 2],
+			[2, 1, 3],
+			[2, 3, 1],
+			[3, 1, 2],
+			[3, 2, 1],
+		])
+	})
+})
