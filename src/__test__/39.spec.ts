bbcfbb15bd21fbce5efc585d07e1ebfbd5c254ac
Jayden12138 <18993882884@163.com>
2024-04-15 14:15:56 +0800

    39

---
+++ src/__test__/39.spec.ts
@@ -0,0 +1,7 @@
+import { combinationSum } from '../../39.组合总和'
+
+describe('first', () => {
+	it('combinationSum', () => {
+		expect(combinationSum([2, 3, 6, 7], 7)).toEqual([[2, 2, 3], [7]])
+	})
+})
