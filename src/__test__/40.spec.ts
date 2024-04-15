602a8b6f055203526897d092d552fc44679585a2
Jayden12138 <18993882884@163.com>
2024-04-15 09:27:19 +0800

    40

---
+++ src/__test__/40.spec.ts
@@ -0,0 +1,12 @@
+import { combinationSum2 } from '../../40.组合总和-ii'
+
+describe('first', () => {
+	it('combinationSum2', () => {
+		expect(combinationSum2([10, 1, 2, 7, 6, 1, 5], 8)).toEqual([
+			[1, 1, 6],
+			[1, 2, 5],
+			[1, 7],
+			[2, 6],
+		])
+	})
+})
