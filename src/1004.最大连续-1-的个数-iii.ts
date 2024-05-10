944777a89788d6999006e072b2c07928aef1d759
Jayden <18993882884@163.com>
2024-05-10 10:31:17 +0800

    feat: 1004

--- src/1004.最大连续-1-的个数-iii.ts
+++ src/1004.最大连续-1-的个数-iii.ts
@@ -10,54 +10,41 @@ function longestOnes(nums: number[], k: number): number {
 	// 输出：6
 	// 解释：[1,1,1,0,0,1,1,1,1,1,1]
 	// 粗体数字从 0 翻转到 1，最长的子数组长度为 6。
-
+	//
 	// 什么时候扩大窗口
 	// 什么时候缩小窗口
 	// 什么时候得到一个合法的答案
-
+	//
 	// 可替换次数 大于等于 0 扩大
 	// 可替换次数 小于 0 缩小
 	// 当可替换次数大于等于0时，窗口内都是1
 
-	if (!nums.length) return 0
-
-	const window = new Map()
+	let res = 0
 
 	let left = 0
 	let right = 0
-	let len = -Infinity
-	let canChangeCount = k
+
+	let windowOneCount = 0
 
 	while (right < nums.length) {
 		let add = nums[right]
+		if (add === 1) {
+			windowOneCount++
+		}
 		right++
 
-		if (canChangeCount >= 0) {
-			window.set(right, add)
-			if (add === 0) {
-				canChangeCount--
-			}
-
-			// console.log(len, window)
-		}
-
-		while (canChangeCount < 0) {
-			// console.log('shake: ', window)
+		while (right - left - windowOneCount > k) {
 			let remove = nums[left]
-			left++
-
-			if (remove === 0) {
-				canChangeCount++
+			if (remove === 1) {
+				windowOneCount--
 			}
-			window.delete(left)
+			left++
 		}
 
-		if (canChangeCount >= 0) {
-			len = Math.max(len, window.size)
-		}
+		res = Math.max(res, right - left)
 	}
 
-	return len
+	return res
 }
 // @lc code=end
 export { longestOnes }
